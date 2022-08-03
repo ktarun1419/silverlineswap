90c84365663e1c73d1da829d2edf481e45a88d6e
tarun kaushik <86646398+ktarun1419@users.noreply.github.com>
2022-08-03 21:37:41 +0530

    Add files via upload

--- src/mixins/metamask.ts
+++ src/mixins/metamask.ts
@@ -2,71 +2,72 @@ import store from "@/store";
 import Web3 from "web3/dist/web3.min.js";
 import Web3Modal, { CHAIN_DATA_LIST, Provider } from "web3modal";
 import { Vue, Options } from "vue-class-component";
+import WalletConnectProvider from "@walletconnect/web3-provider";
 @Options({})
-export default class Metamask extends Vue{
+export default class Metamask extends Vue {
+
+  public async provider() {
     
-    public async provider() {
-      const globalAny:any = globalThis;
-      const WalletConnectProvider = globalAny.WalletConnectProvider;
-      let providerOptions = {
-        walletconnect: {
-          package: WalletConnectProvider,
-          options: {
-            rpc: {
+    let providerOptions = {
+      walletconnect: {
+        package: WalletConnectProvider,
+        options: {
+          rpc: {
             1: "https://mainnet.infura.io/v3/b50bee145172497d9576a6f79b1209aa",
           },
-          }
-        },
-      };
-       
-        let web3modal: any = new Web3Modal({
-            network: "mainnet",
-            cacheProvider: false,
-            providerOptions
-          
+        }
+      },
+    };
+
+    let web3modal: any = new Web3Modal({
+      network: "mainnet",
+      cacheProvider: false,
+      providerOptions,
+
+    });
+    console.log('function called')
+    let provider: any = await web3modal.connect();
+    store.commit("set_Provider", provider);
+    let web3: any = new Web3(provider);
+    let account: any = web3.eth.getAccounts();
+    account.then((result) => {
+      store.commit("set_WalletAddress", result[0])
+      let account: string;
+      if (result[0] != null) {
+        let nim = result[0].split("");
+        account =
+          nim[0] +
+          nim[1] +
+          nim[2] +
+          nim[3] +
+          "..." +
+          nim[37] +
+          nim[38] +
+          nim[39] +
+          nim[40] +
+          nim[41]
+        store.commit("set_TopbarAcc", account);
+      }
+    })
+    let chain = web3.eth.getChainId();
+    chain.then((result) => {
+      console.log(result);
+
+      if (result == 4) {
+        store.commit("set_chainId", result)
+      }
+      else {
+        try {
+          const web3 = new Web3(window.ethereum)
+          web3.currentProvider.request({
+            method: 'wallet_switchEthereumChain',
+            params: [{ chainId: Web3.utils.toHex(56) }],
           });
-          console.log('function called')
-          let provider: any = await web3modal.connect();
-          store.commit("set_Provider", provider);
-          let web3: any = new Web3(provider);
-          let account: any =  web3.eth.getAccounts();
-          account.then((result)=>{
-              store.commit("set_WalletAddress",result[0])
-            let account: string;
-            if (result[0] != null) {
-              let nim = result[0].split("");
-              account =
-                nim[0] +
-                nim[1] +
-                nim[2] +
-                nim[3]+
-                "..." +
-                nim[37] +
-                nim[38] +
-                nim[39] +
-                nim[40]
-              store.commit("set_TopbarAcc", account);
-            }
-          })
-          let chain = web3.eth.getChainId();
-              chain.then((result) => {
-                console.log(result);
-                
-                if (result==4) {
-                    store.commit("set_chainId", result)
-                }
-                else{
-                  try {
-                     const web3=new Web3(window.ethereum)
-                      web3.currentProvider.request({
-                       method: 'wallet_switchEthereumChain',
-                         params: [{ chainId: Web3.utils.toHex(56) }],
-                       });
-                       store.commit("set_chainId", result)
-                   } catch (switchError) {
-                     // This error code indicates that the chain has not been added to MetaMask.
-                   }
-                }
-              })
-    }
+          store.commit("set_chainId", result)
+        } catch (switchError) {
+          // This error code indicates that the chain has not been added to MetaMask.
+        }
+      }
+    })
+  }
 }
