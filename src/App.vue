bfbc7905de86ff6b8d79b23ae74937b5da297502
tarun kaushik <86646398+ktarun1419@users.noreply.github.com>
2022-08-05 12:12:16 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -18,8 +18,6 @@ export default class App extends Vue{
 #app {
    //font-family: Avenir, Helvetica, Arial, sans-serif;
   font-family: poppins;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   font-weight: 300;
   position: absolute;
